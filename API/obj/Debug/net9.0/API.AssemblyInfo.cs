c6bf3d02d568809e1fa72d5c5cabc501bfd56783
manvendra2291 <manvendra2291@gmail.com>
2025-07-14 23:31:23 +0530

    Section 4:Creating a Crud Application in react

--- API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93586b5a0306d5cbd28204681dffe89a620a45a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+76930d2f8ed558c2dfc7c031189e0c033f481c25")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
